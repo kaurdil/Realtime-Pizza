a2ccd47f8196eb4634e867ecbb94db8f7a7da727
kaurdil <kaurdilpreet116@gmail.com>
2021-05-31 00:32:13 +0530

    implemented authentication system

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,31 +1,31 @@
 import axios from 'axios'
 import Noty from 'noty'
 
-let addToCart=document.querySelectorAll('.add-to-cart');
-var cartCounter=document.querySelector('#cartCounter');
-function updateCart(foods){
-    axios.post('/update-cart',foods).then(res=>{
-        cartCounter.innerText=res.data.totalQty;
+let addToCart = document.querySelectorAll('.add-to-cart')
+let cartCounter = document.querySelector('#cartCounter')
+
+function updateCart(foods) {
+    axios.post('/update-cart', foods).then(res => {
+        cartCounter.innerText = res.data.totalQty
         new Noty({
-            type:'success',
-            timeout:1000,
+            type: 'success',
+            timeout: 1000,
             text: 'Item added to cart',
-            progressBar:false
-            
+            progressBar: false,
         }).show();
-    }).catch(err=>{
+    }).catch(err => {
         new Noty({
-            type:'error',
-            timeout:1000,
+            type: 'error',
+            timeout: 1000,
             text: 'Something went wrong',
-            progressBar:false
-            
+            progressBar: false,
         }).show();
     })
 }
-addToCart.forEach((btn)=>{
-   btn.addEventListener('click',(e)=>{
-       let foods=JSON.parse(btn.dataset.foods);
-       updateCart(foods);
-   })
+
+addToCart.forEach((btn) => {
+    btn.addEventListener('click', (e) => {
+        let foods = JSON.parse(btn.dataset.foods)
+         updateCart(foods)
+    })
 })
--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const path=require('path');
 const mongoose=require('mongoose');
 const session=require('express-session');
 const flash=require('express-flash');
+const passport=require('passport');
 const MongoDbStore=require('connect-mongo')(session)
 
 //database Connection
@@ -15,6 +16,8 @@ mongoose.connect('mongodb://localhost/Food', function (err) {
 
 const app=express();
 const PORT=process.env.PORT||3000;
+
+
 
 //session store
 let mongoStore=new MongoDbStore({
@@ -30,15 +33,25 @@ app.use(session({
     saveUninitialized:false,
     cookie:{ maxAge : 1000*15 }
 }))
+//Passport config
+const passportInit=require('./app/config/passport')
+passportInit(passport)
+app.use(passport.initialize())
+app.use(passport.session())
 app.use(flash());
+//Assets
+app.use(express.static('public'))
+app.use(express.urlencoded({extended:false}))
+app.use(express.json())
+
 //Global middleware
 app.use((req,res,next)=>{
    res.locals.session=req.session
+   res.locals.user=req.user
    next()
 })
-//Assets
-app.use(express.static('public'))
-app.use(express.json())
+
+
 // set template engine
 app.use(expressLayout);
 app.set('views',path.join(__dirname,'/resources/views'))
